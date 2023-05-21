f2ff46509dea55340c6c3b44f5d97dba561e7263
Naimur178 <naim.cse17@gmail.com>
2023-05-21 23:16:13 +0600

    header banner motto updated

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                         <section className="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80')] from-{bg-gray-900} to-{bg-gray-100} bg-pink-300 bg-blend-multiply w-full">
                             <div className="px-4 w-1/2   py-24 lg:py-56 ">
                                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Play With Heros</h1>
-                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Chinese food boasts mouth-watering flavors, savory stir-fries, fragrant rice dishes, delicious dumplings, and tender meat dishes.</p>
+                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Unleash the Power of Imagination with Action Figure Adventures!</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row  sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium  text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                                         order Now
@@ -29,8 +29,8 @@ const Header = () => {
                     <div id="slide2" className="carousel-item relative w-full">
                         <section className="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1558507334-57300f59f0bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-pink-300 bg-blend-multiply w-full">
                         <div className="px-4 w-1/2   py-24 lg:py-56 ">
-                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Play With Heros</h1>
-                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Chinese food boasts mouth-watering flavors, savory stir-fries, fragrant rice dishes, delicious dumplings, and tender meat dishes.</p>
+                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl">Play With Heros</h1>
+                                <p className="mb-8 text-lg font-normal  lg:text-xl">Unleash the Power of Imagination with Action Figure Adventures!</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row  sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium  text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                                         order Now
@@ -49,7 +49,7 @@ const Header = () => {
                         <section className="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1630710478039-9c680b99f800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-pink-300  bg-blend-multiply w-full">
                         <div className="px-4 w-1/2   py-24 lg:py-56 ">
                                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Play With Heros</h1>
-                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Chinese food boasts mouth-watering flavors, savory stir-fries, fragrant rice dishes, delicious dumplings, and tender meat dishes.</p>
+                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Unleash the Power of Imagination with Action Figure Adventures!</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row  sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium  text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                                         order Now
@@ -68,7 +68,7 @@ const Header = () => {
                         <section className="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1559339484-2205377cc979?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1228&q=80')] bg-pink-300  bg-blend-multiply w-full">
                         <div className="px-4 w-1/2   py-24 lg:py-56 ">
                                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Play With Heros</h1>
-                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Chinese food boasts mouth-watering flavors, savory stir-fries, fragrant rice dishes, delicious dumplings, and tender meat dishes.</p>
+                                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl">Unleash the Power of Imagination with Action Figure Adventures!.</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row  sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium  text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                                         order Now
